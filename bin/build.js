e6559d2e4cfa80e126fadf9ce3f5df989405db98
TBXark <tbxark@outlook.com>
2022-04-19 10:20:05 +0800

    remove zx/serve dependency

--- bin/build.js
+++ bin/build.js
@@ -1,7 +1,5 @@
-import { argv, cd, fs } from "zx";
-import "zx/globals";
 
-cd(argv.d || ".")
+const fs = require("fs");
 
 let indexHTML = fs.readFileSync("index.html", "utf8");
 
@@ -14,7 +12,7 @@ const jsUpdateTime = fs.statSync("./src/ternimal.js").mtimeMs / 1000;
 // <link href="./src/home.css?ts=__STYLE_TIME_STAMP__" rel="stylesheet" />
 // <script src="./sr/ternimal.js?ts=__SCRIPT_TIME_STAMP__" defer ></script>
 
-indexHTML = indexHTML.replace(/__STYLE_TIME_STAMP__/g, parseInt(cssUpdateTime));
-indexHTML = indexHTML.replace(/__SCRIPT_TIME_STAMP__/g, parseInt(jsUpdateTime));
+indexHTML = indexHTML.replace("__STYLE_TIME_STAMP__", parseInt(cssUpdateTime));
+indexHTML = indexHTML.replace("__SCRIPT_TIME_STAMP__", parseInt(jsUpdateTime));
 
 fs.writeFileSync("index.html", indexHTML);
